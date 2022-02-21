bafbe772ddd2d645c1467541ce762edbb9bd39df
Andrey Kryuchkov <askryuchkov@ya.ru>
2022-02-21 12:43:12 +0800

    stupid questions ex done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    if params[:question] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:question].include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
+  end
+end
